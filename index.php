67dd342d69d5cc26e74614ea3b4295599710c058
charlotte-sch <84276466+charlotte-sch@users.noreply.github.com>
2022-03-23 14:50:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+// inclure le head
+include("views/html/head.php");
+
+// inclure le router
+include("controller/router.php");
+
+// inclure le footer
+include("views/html/footer.php");
+?>
